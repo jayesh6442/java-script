c10a84c198aee28a63f14a2661ee36908254b10e
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2024-01-05 18:46:42 +0530

    none

--- day1/day1.js
+++ day1/day1.js
@@ -57,8 +57,224 @@ console.log(a**b);*/
 
 // incremetn operator
 
-let a =0
+/*let a =0
 a++
-console.log(a);
+console.log(a);*/
 
 // type like ++a  a++ pre and post
+
+//loops
+
+/*for (let index = 0; index < 5; index++) {
+     console.log("hello javascript");
+    
+}
+//sum of 1 to 100 numbers
+
+let sum =0
+
+for (let index = 0; index < 101; index++) {
+    sum+=index
+}
+console.log(sum);
+
+
+for (let index = 0; index < 100; index++) {
+    console.log(sum);
+    
+}
+let n=1
+while (n< 100) {
+    console.log("while");
+    n++
+    
+}
+namef = "sarvaiya"
+for (const val of namef) {
+    console.log("i= ",val);
+
+}
+
+size = 0
+lname ="hello and welcome to github"
+for (const i of lname) {
+   size+=1
+  
+}
+
+console.log("sting size is ",size);
+
+
+student ={
+    fname:"jayesh",
+    roll:23,
+    mark:56,
+    lname:"sarvaiya"
+}
+for (const key in student) {
+        console.log(key);
+        
+    }
+
+for (let index = 0; index < 100; index++) {
+    if(index%2==0){
+        console.log(index);
+    }
+    
+    
+}*/
+
+
+
+    //practice qu
+/*gameNumb =24
+let userNum = prompt("guss the numb")
+while(userNum!=gameNumb){
+    userNum = prompt("you guees the wrong num")
+}
+console.log("congratulation you guessed right number");*/
+/*let and ="jayeshsarvaiya" 
+console.log(and.length);
+console.log(and[0]);
+console.log(and[1]);
+console.log(and[2]);
+console.log(and[3]);
+console.log(and[4]);
+console.log(and[4]);
+
+let obj ={
+    pen: 19,
+    type: "pen",
+}
+upper = and.toUpperCase();
+
+console.log(upper);
+console.log(`the type is ${obj.type} and price is ${obj.pen}`);*/
+
+
+//array is javascirpt  and lets goooo men
+
+/*let heros = ['jayehs','sarvaiya','git']
+for (const val of heros) {
+    console.log(val.toUpperCase());
+
+    
+}
+
+
+let sum =0
+
+let marks = [34,34,65,67,45,23,54,23]
+for (const mark of marks) {
+    sum +=mark
+    
+}
+let average = sum / marks.length
+
+console.log(sum);
+console.log(average);
+
+
+
+let prise = [34,35,34,54,34,54,34,64]
+
+let i = 0
+for (const val of prise) {
+
+    let offer = val / 10;
+    prise[i] = prise[i]-offer
+    console.log(`value after offer ${prise[i]}`);
+    i++
+}
+console.log(prise);
+console.log(prise.length);
+prise.push(46)
+console.log(prise.length);
+//concat arrar useing cancat()
+
+prise.unshift(34)
+console.log(prise.length);
+
+prise.shift(1)
+console.log(
+    prise.length
+);
+console.log("im having fun");
+
+
+
+function name(){
+    console.log("modi ji he");
+}
+name();
+
+function hello(jana) {
+    console.log(`hello ${jana}`);
+    
+}
+console.log(hello("jayehs"));*/
+
+//function and parameter we having fun lerning the stuff hell yeeee
+
+/*function sum(a,b){
+    return a+b;
+}
+console.log(sum(23,4));
+
+const mul = (a,b)=>{
+    return a*b;
+}
+
+console.log(mul(4,6));
+
+
+function countVouls (str){
+    let count =0
+    for (const char of str) {
+        if (
+            char ==="a" ||
+            char ==="e" ||
+            char ==="i" ||
+            char ==="o" ||
+            char ==="u"
+            ) {
+            count++
+        }
+        
+        
+    }
+    console.log(`number of vouls is ${count}`);
+}
+
+console.log(countVouls("jayesh sarivaiya"));
+
+let nan = [2,3,4,4,5,3,4,5];
+nan.forEach(function printVal(z){
+    console.log(z);
+
+});
+
+
+//map function
+
+let copy = nan.map((val)=>{
+        return val;
+
+})
+console.log(copy);
+let what = nan.filter(( val)=>{
+    return val %2==0
+})
+console.log(what);*/
+
+
+
+// DOM and DOM manipulation and here we gooooooooooo
+
+
+/*
+document.querySelector("name");
+console.log();
+
+window.alert("hello jayesh")*/
+
