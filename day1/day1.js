b2bac6c4aacb81b637359487f009f4599a6117f4
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2024-01-05 13:45:50 +0530

    none

---
+++ day1/day1.js
@@ -0,0 +1,29 @@
+console.log("hello nigger");
+
+let n =20
+console.log(n);
+
+console.log("jayesh");
+
+const age = 13
+console.log(age);
+let name= "tony stark"
+console.log(name);
+fname = 'sarvaiya'
+console.log(fname);
+
+x = null 
+console.log(x);
+
+y= undefined
+console.log(y);
+
+name_1="jayesh"
+name_2="sarvaiya"
+
+console.log(name_1  + " " +  name_2);
+isFollow= true
+console.log(isFollow);
+
+
+
