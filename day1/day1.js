b752b38fba03df0f9756f05f20af8cb8c1339578
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2024-01-05 14:04:41 +0530

    addet

--- day1/day1.js
+++ day1/day1.js
@@ -1,29 +1,64 @@
-console.log("hello nigger");
+/*console.log("hello nigger");
 
 let n =20
 console.log(n);
-
 console.log("jayesh");
-
 const age = 13
 console.log(age);
 let name= "tony stark"
 console.log(name);
 fname = 'sarvaiya'
 console.log(fname);
-
 x = null 
 console.log(x);
-
 y= undefined
 console.log(y);
-
 name_1="jayesh"
 name_2="sarvaiya"
-
 console.log(name_1  + " " +  name_2);
 isFollow= true
 console.log(isFollow);
+// let and connt for variable declaration  old way was var
+console.log(typeof(name_1));
+ 
+a= Symbol("hello");
+console.log(typeof(a));
+const student = {
+    fullName: "jayesh sarvaiya",
+    age: 23,
+    cgpa: 24,
+    isPass:true,
+}
+console.log(student);
+console.log(typeof(student));
+console.log(student["fullName"]);
+console.log(student.age);
+console.log(student.cgpa);
+console.log(student.isPass); */
+/* operatos and condition operators
++ - * /  this are the operatos in javascript this operators remain same in all the programming languages*/
+ 
+
+// arithmetic operatos
+
+/*let a = 3
+let b = 3
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a/b);
+ // module %
+ let z =4
+ let y = 8
+ console.log(z%y);
+ //double multiplication **
+console.log(a**b);*/
 
 
+// incremetn operator
 
+let a =0
+a++
+console.log(a);
+
+// type like ++a  a++ pre and post
